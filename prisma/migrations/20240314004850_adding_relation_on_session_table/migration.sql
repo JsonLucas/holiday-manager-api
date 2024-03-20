19f0901a910057b6874a9dfc5a6fd4b1621407f5
jason <jasonlucas443@gmail.com>
2024-03-20 18:38:54 -0300

    adding configuration for vercel deploy

--- prisma/migrations/20240314004850_adding_relation_on_session_table/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE `Session` ADD CONSTRAINT `Session_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/migrations/20240319163554_adding_ondelete_restriction/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `task` DROP FOREIGN KEY `Task_holiday_id_fkey`;
-
--- AddForeignKey
-ALTER TABLE `Task` ADD CONSTRAINT `Task_holiday_id_fkey` FOREIGN KEY (`holiday_id`) REFERENCES `Holiday`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
